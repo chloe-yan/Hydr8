874f074248ba459c8d07d756b52b12ad815250c4
chloe-yan <chloeyan365@gmail.com>
2020-02-28 22:40:03 -0800

    Add UIBezierPath

---
+++ Hydra/UIBezierPathExtension.swift
@@ -0,0 +1,24 @@
+//
+//  UIBezierPathExtension.swift
+//  Hydra
+//
+//  Created by Chloe Yan on 2/13/20.
+//  Copyright © 2020 Chloe Yan. All rights reserved.
+//
+
+import UIKit
+
+extension UIBezierPath {
+    convenience init(curvedSegment: CurvedSegment) {
+        self.init()
+        self.move(to: curvedSegment.startPoint)
+        self.addCurve(to: curvedSegment.toPoint, controlPoint1: curvedSegment.controlPoint1, controlPoint2: curvedSegment.controlPoint2)
+        self.addLine(to: curvedSegment.endPoint)
+    }
+    
+    convenience init(lineSegment: LineSegment) {
+        self.init()
+        self.move(to: lineSegment.startPoint)
+        self.addLine(to: lineSegment.endPoint)
+    }
+}
